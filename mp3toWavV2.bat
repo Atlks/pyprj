766a1b58565a6cba4c828839319c73c7c2d35d22
domas <domas@domas>
2025-02-03 11:12:23 +0700

    。。

--- mp3toWavV2.bat
+++ mp3toWavV2.bat
@@ -2,3 +2,7 @@ rem for %i in (*.mp3) do ffmpeg -i "%i" "%~ni.wav"
 
 ffmpeg  -i 网络连接警告.mp3 网络连接警告.mp3.wav
 ffmpeg  -i 星标联系人消息.mp3  星标联系人消息.mp3.wav
+打卡警告.mp3
+
+ffmpeg  -i 打卡警告.mp3  打卡警告.mp3.wav
+ffmpeg  -i  网络连接警报.mp3  网络连接警报.mp3.wav
